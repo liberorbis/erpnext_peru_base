9eb89b38e5221924944844734792112e23caf04e
LPA <luisparedesa@liberorbis.com>
2015-03-03 11:16:09 -0500

    doctypes tables sunat

---
+++ erpnext_peru_base/erpnext_peru_base/doctype/table_sunat_01/test_table_sunat_01.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2013, dsdf and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Table Sunat 01')
+
+
+class TestTableSunat01(unittest.TestCase):
+    pass
