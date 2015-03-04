b8e6b9464d5e904523285fe6dce00d5bbe1cee0f
kbejarano <kbejarano@liberorbis.com>
2015-03-04 09:21:12 -0500

    doctypes for address peru

--- erpnext_peru_base/erpnext_peru_base/doctype/address_department/address_department.py
+++ erpnext_peru_base/erpnext_peru_base/doctype/address_department/address_department.py
@@ -1,10 +1,9 @@
-# Copyright (c) 2013, Peru and contributors
+# Copyright (c) 2013, dsdf and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class Control_Series(Document):
+class AddressDepartment(Document):
 	pass
-
---
+++ erpnext_peru_base/erpnext_peru_base/doctype/address_department/test_address_department.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2013, dsdf and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Address Department')
+
+class TestAddressDepartment(unittest.TestCase):
+	pass
