e98d72dc8e42d0b1f6ffaa4c1143bb4fafb7d3ce
kbejarano <kbejarano@liberorbis.com>
2015-03-25 18:03:20 -0500

    tipo de cambio

--- erpnext_peru_base/erpnext_peru_base/doctype/exchange_rate/exchange_rate.py
+++ erpnext_peru_base/erpnext_peru_base/doctype/exchange_rate/exchange_rate.py
@@ -5,34 +5,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-from frappe.celery_app import celery_task, task_logger
-
-
 class ExchangeRate(Document):
-    pass
-
-
-@celery_task()
-def fill_rate(site, exchangerate, event):
-    # hack! pass event="bulk_long" to queue in longjob queue
-    try:
-        frappe.connect(site=site)
-        doc = frappe.get_doc("Exchange Rate", exchangerate)
-        doc.send_bulk()
-
-    except:
-        frappe.db.rollback()
-        task_logger.warn(frappe.get_traceback())
-
-        # wasn't able to send emails :(
-        doc.db_set("email_sent", 0)
-        frappe.db.commit()
-
-        raise
-
-    else:
-        frappe.db.commit()
-
-    finally:
-        frappe.destroy()
-
+	pass
